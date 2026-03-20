9eff8988fa3ad05c79a3872addd89af0e2045696
SakuraVillager <SakuraVillager@outlook.com>
2026-03-20 16:13:24 +0800

    修复人物仓储类型定义bug，更新依赖包源为国内镜像

--- lib/data/celebrity_repository.dart
+++ lib/data/celebrity_repository.dart
@@ -1,20 +1,21 @@
 import '../models/celebrity_profile.dart';
 import 'supabase_table_repository.dart';
 
-typedef _CelebrityFetcher = Future<List<CelebrityProfile>> Function();
+typedef CelebrityFetcher = Future<List<CelebrityProfile>> Function();
 
 /// 人物仓储：
 /// - 对外提供强类型 `CelebrityProfile`
 /// - 底层复用通用 `SupabaseTableRepository`
 class CelebrityRepository {
   const CelebrityRepository({
-    _CelebrityFetcher? fetcher,
+    CelebrityFetcher? fetcher,
     SupabaseTableRepository? tableRepository,
   }) : _fetcher = fetcher,
        _tableRepository =
-           tableRepository ?? const SupabaseTableRepository(tableName: 'Celebrity');
+           tableRepository ??
+           const SupabaseTableRepository(tableName: 'Celebrity');
 
-  final _CelebrityFetcher? _fetcher;
+  final CelebrityFetcher? _fetcher;
   final SupabaseTableRepository _tableRepository;
 
   Future<List<CelebrityProfile>> fetchCelebrities() async {
